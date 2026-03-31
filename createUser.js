0f69e55d2b6ef5c9b9f03c30bf218b2dc2ae4dd3
GTCSYS Backend Dev System v3 <gtcsysdeveloper@gmail.com>
2026-03-31 15:28:27 +0530

    commit of 31st

--- createUser.js
+++ createUser.js
@@ -62,4 +62,36 @@ async function createUser2(token, user) {
   //hey this is second testing i am going to push 
 }
 
+//this is dummmy function just to make pull test for 31st commit
+async function createUser2(token, user) {
+  await axios.post(
+    `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.REALM}/users`,
+    {
+      username: user.email,
+      email: user.email,
+      enabled: true,
+      emailVerified: true,
+
+      firstName: user.firstName,
+      lastName: user.lastName,
+
+      // Attributes MUST be strings
+      attributes: {
+        legacyUserId: String(user.legacyUserId),   // ✅ fixed
+        migrated: "true",                  // ✅ added
+        passwordInitialized: "false"       // ✅ fixed
+      }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+  //hey this is second commit message testing 
+  //hey this is second testing i am going to push 
+}
+
+
 module.exports = createUser;
